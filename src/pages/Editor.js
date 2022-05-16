5766bc10ff27ade74b94f6537c02da0e4dd2915d
YoommY <seeun0792@gmail.com>
2022-05-17 02:25:10 +0900

    feat : Title 입력 기능

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,9 +1,16 @@
 //메인 화면에서 글쓰기 버튼을 클릭하면 새로 작성->확인 클릭시 디테일 화면에 매핑됨
 //디테일 페이지에서 클릭하면 기존에 작성된 내용을 에디터 페이지로 불러오기+삭제
 
+import { useCallback, useState} from "react";
 import styled from "styled-components";
 
 function Editor(){
+
+    const [title, setTitle] = useState(""); //useState를 활용해서 title 관리
+  
+    const onChange = ((event) => {
+        setTitle(event.target.value)
+        console.log(title)});
 
     const Container = styled.div`
     
@@ -15,11 +22,15 @@ function Editor(){
     font-size:30px;
     `
 
+    const Form = styled.form`
+    
+    padding:10px;
+    `
     const PostingButton = styled.button`
     
     font-size:15px;
     `
-    const Title = styled.div`
+    const Title = styled.input`
     
     font-size:15px;
     border-bottom:1px solid grey;
@@ -30,17 +41,23 @@ function Editor(){
     font-size:15px;
     border-bottom:1px solid grey;
     padding:5px;
+
     `
 
     return(
         <>
-        <Container>
-            <Header>블로그 글쓰기</Header>
-            <PostingButton>포스트 수정</PostingButton>
-            <Title>제목</Title>
+            <Form>
+                <Title
+                type="text"
+                value={title}
+                onChange={onChange}
+                placeholder="제목"
+                spellCheck="false"
+                autoFocus = {true}
+            /> 
             <Content>본문</Content>
-        </Container>
-        
+            <PostingButton>포스트 수정</PostingButton>
+            </Form>
         </>
     );
     
