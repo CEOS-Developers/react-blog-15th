d0d462b5017bdad09121f5e33562f4e246eab0d1
YoommY <seeun0792@gmail.com>
2022-05-18 01:16:57 +0900

    chore : 상세페이지에도 recoil 적용

--- pages/DetailedContainer/[id].js
+++ pages/DetailedContainer/[id].js
@@ -1,14 +1,17 @@
 import {useRouter} from 'next/router';
 import DetailedPost from '../../src/components/DetailedPost';
 import data from '../../src/data/data.json';
+import { useRecoilValue } from 'recoil';
+import { PostListState } from '../../src/recoil/recoil';
 
 function DetailContainer(){
     
     const router = useRouter();
     const {id} = router.query;
     const numbering = Number(id);
+    const currentPostList = useRecoilValue(PostListState);
 
-    const [postObj] = data.filter((post) => post.id === numbering); //id를 이용해서 post filter
+    const [postObj] = currentPostList.filter((post) => post.id === numbering); //id를 이용해서 post filter
 
     const title = postObj.title; //각 내용들 변수에 저장
     const content = postObj.content;
