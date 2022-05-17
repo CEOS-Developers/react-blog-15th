d14c1b22436013effdc48771b17e0a9663fcf500
usernamebuzz <ohminji@ewhain.net>
2022-05-17 10:30:33 +0900

    Chore: Separate post from screen

--- pages/Details.js
+++ pages/Details.js
@@ -1,2 +1,24 @@
 //메인화면에서 클릭하면 제목, 작성일, 본문의 내용을 표시
 //여긴 내가 따로 구성하면 어려워질 것 같아서 일단 내버려둘게용
+
+//상세보기 화면에서 나타나는 포스트
+
+import styled from "styled-components";
+import React,{ useState } from "react";
+import { useParams } from 'react-router-dom';
+
+import data from "../data/data.json"
+
+const Details = ({ title,date, content }) => {
+    const { milisec } = useParams();
+    const [postObj] = data.filter((post) => post.milisec === milisec);
+    const [currentPost, setcurrentPost] = useState(postObj);
+
+    return (
+      <PostWrapper title={currentPost.title} date={currentPost.date} content={currentPost.content}>
+      {title} {date} {content}
+      </PostWrapper>
+);
+  }
+
+  export default Details;
--- src/components/DetailsPost.js
+++ src/components/DetailsPost.js
@@ -1,21 +1,20 @@
-//상세보기 화면에서 나타나는 포스트
+//메인화면에서 보이는 포스트
 
 import styled from "styled-components";
-import React,{ useState } from "react";
-import { useParams } from 'react-router-dom';
+import React from "react";
 
-import data from "../data/data.json"
-
-const DetailPost = ({ title,date, content }) => {
-    const { milisec } = useParams();
-    const [postObj] = data.filter((post) => post.milisec === milisec);
-    const [currentPost, setcurrentPost] = useState(postObj);
-
+const DetailsPost = ({ title,date,content }) => {
     return (
-      <PostWrapper title={currentPost.title} date={currentPost.date} content={currentPost.content}>
+      <PostWrapper title={title} date={date} content={content}>
       {title} {date} {content}
       </PostWrapper>
 );
   }
 
-  export default DetailPost;
+const PostWrapper = styled.section`
+    
+    font-size:15px;
+    border-bottom:1px solid grey;
+    padding:5px;
+    `
+export default DetailsPost;
