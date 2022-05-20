73d1651f5d04e5f57f27d3d60b6a71bfa46bb2d3
usernamebuzz <ohminji@ewhain.net>
2022-05-20 10:27:31 +0900

    Fix: Alert when inputText is empty

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "46de8835ea090292"; }
+/******/ 		__webpack_require__.h = function() { return "0d21a114ca7402d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/EditorContainer.tsx
+++ src/components/EditorContainer.tsx
@@ -17,30 +17,30 @@ import React from 'react';
 function EditorContainer() {
   const { inputText, onInputChange, reset } = useInput();
   const [postList, setPostList] = useRecoilState(PostListState);
-  
+
   const navigateHome = useNavigateHome();
   const numbering = useRouting();
 
   const [postObj] = postList.filter((post) => post.postNum === numbering);
 
-  const savedTitle = numbering ? postObj.title : "";
-  const savedContent = numbering ? postObj.content : "";
+  const savedTitle = numbering ? postObj.title : '';
+  const savedContent = numbering ? postObj.content : '';
   const newDate = getInputDate();
 
-  const handleNewPost = ((e : React.SyntheticEvent) => {
+  const handleNewPost = (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    if (inputText) {
+    if (inputText.title != '' || inputText.content != '') {
       const post = {
-        postNum : numbering ? numbering : postList.length+1,
+        postNum: numbering ? numbering : postList.length + 1,
         title: inputText.title,
         content: inputText.content,
         date: numbering ? postObj.date : newDate,
         id: numbering ? postObj.id : Date.now(),
       };
 
-      const newArray = [...postList]; 
-      newArray.splice(numbering-1,1,post); //포스트 수정 
+      const newArray = [...postList];
+      newArray.splice(numbering - 1, 1, post); //포스트 수정
       setPostList(numbering ? newArray : (postList) => [...postList, post]); //포스트 수정,포스트 등록
 
       reset();
@@ -48,33 +48,29 @@ function EditorContainer() {
       alert('입력하세요!');
     }
     navigateHome();
-  });
+  };
 
   return (
     <CenteringWrapper>
-      <Header>{numbering ? "글 수정하기" : "글 등록하기"}</Header>
+      <Header>{numbering ? '글 수정하기' : '글 등록하기'}</Header>
       <Container>
         <Form>
           <InputWrapper>
             <Title
               name="title"
               defaultValue={savedTitle}
-              value={inputText.title}
               onChange={onInputChange}
-              placeholder={savedTitle}
               spellCheck="false"
             />
             <Content
               name="content"
               defaultValue={savedContent}
-              value={inputText.content}
               onChange={onInputChange}
-              placeholder={savedContent}
               spellCheck="false"
             />
           </InputWrapper>
           <PostHandleButton onClick={handleNewPost}>
-          {numbering ? "포스트 수정" : "포스트 등록"}
+            {numbering ? '포스트 수정' : '포스트 등록'}
           </PostHandleButton>
         </Form>
       </Container>
