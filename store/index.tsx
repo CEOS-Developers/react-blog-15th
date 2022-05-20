0036953ecc0e363a1ee35e13af9e0cc271e47cbe
24siefil <24siefil@gmail.com>
2022-05-20 15:00:10 +0900

    chore: RTK logger middleware 추가, RootState, AppDispatch type export 코드 추가

--- store/index.tsx
+++ store/index.tsx
@@ -1,21 +1,25 @@
+import logger from 'redux-logger';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 import posts from './modules/postsSlice';
-const reducer = (state, action) => {
+
+export const reducer = (state, action) => {
   if (action.type === HYDRATE) {
     return { ...state, ...action.payload };
   }
   return combineReducers({ posts })(state, action);
 };
 
-const makeStore = () =>
-  configureStore({
-    reducer,
-  });
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+const makeStore = () => store;
 
 export const wrapper = createWrapper(makeStore, {
   debug: process.env.NODE_ENV !== 'production',
 });
 
-//export type RootState = ReturnType<typeof store.getState>;
-//export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
